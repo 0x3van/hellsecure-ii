2a08cbaa0cd7099cf3c4409eda9803a8b33451d3
evan <97516411+0x3van@users.noreply.github.com>
2022-05-31 01:59:03 +0000

    little memey shit

--- src/input.lua
+++ src/input.lua
@@ -1,7 +1,8 @@
 -- Source to be obfuscated
-local n = 0
-while wait() and n <= 10 do
-    print(n)
-    n = n + 0.1
+local list = {a}
+for i,v in ipairs(list) do
+  print(list[1])
 end
-print(n)
+-- ? -- ? -- ?
+
+-- no integer overflow
