75578055b0d36cb7ce7b4f337f8f5678e2e9ff8d
evannnns <alt1@trickster.pw>
2022-07-06 12:02:13 +0800

    Update ltn12.lua

--- src/ltn12.lua
+++ src/ltn12.lua
@@ -1,12 +1,14 @@
-local ltn12 = require('ltn12')
+-- ltn12 sink tables testing
 
-ltn12.sink.table = {
-  ["fart"] "foo"
-}
-
-for i,v in ipairs(table) do 
-  if (__index(v) and table[1] == __newindex(i)) then 
-    print()
-    os.execute('exit')
-  end
-end
+-- local ltn12 = require('ltn12')
+-- 
+-- ltn12.sink.table = {
+--   ["fart"] "foo"
+-- }
+-- 
+-- for i,v in ipairs(table) do 
+--   if (__index(v) and table[1] == __newindex(i)) then 
+--     print()
+--     os.execute('exit')
+--   end
+-- end
